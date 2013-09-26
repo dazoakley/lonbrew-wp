19e3429b039e23a2c62322bbb1df678850765c56
Darren Oakley <daz.oakley@gmail.com>
2013-09-26 17:51:14 +0100

    Add a featured post section to the homepage

--- front-page.php
+++ front-page.php
@@ -3,15 +3,26 @@
 <div class="row">
   <div class="span8">
 
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div id="featured" class="well">
+      <?php query_posts('posts_per_page=1&cat=23'); ?>
+      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+        <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+        <p><?php the_excerpt(); ?></p>
+        <p><a href="<?php the_permalink(); ?>">read more &raquo;</a></p>
+      <?php endwhile; ?>
+      <?php wp_reset_query(); ?>
+    </div><!-- /.featured -->
 
-    <?php the_content(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-  <?php endwhile; else: ?>
-    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-  <?php endif; ?>
+      <?php the_content(); ?>
+
+    <?php endwhile; else: ?>
+      <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+    <?php endif; ?>
 
   </div>
+
   <div class="span4">
     <?php get_sidebar(); ?>
   </div>
