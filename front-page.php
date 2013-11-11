4befd195d7750b91e7eac9cf2fe2fdff670eca2a
Darren Oakley <daz.oakley@gmail.com>
2013-11-11 13:44:51 +0000

    Fix featured posts.

--- front-page.php
+++ front-page.php
@@ -3,15 +3,17 @@
 <div class="row">
   <div class="span8">
 
-    <div id="featured" class="well">
-      <?php query_posts('posts_per_page=1&cat=23'); ?>
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-        <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-        <p><?php the_excerpt(); ?></p>
-        <p><a href="<?php the_permalink(); ?>">read more &raquo;</a></p>
-      <?php endwhile; ?>
-      <?php wp_reset_query(); ?>
-    </div><!-- /.featured -->
+    <?php query_posts('posts_per_page=1&cat=23'); ?>
+    <?php if ( have_posts() ) ?>
+      <div id="featured" class="well">
+        <?php while ( have_posts() ) : the_post(); ?>
+          <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+          <p><?php the_excerpt(); ?></p>
+          <p><a href="<?php the_permalink(); ?>">read more &raquo;</a></p>
+        <?php endwhile; ?>
+      </div><!-- /.featured -->
+    <?php endif; ?>
+    <?php wp_reset_query(); ?>
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
